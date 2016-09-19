256e61b8f34ba89a153523697ea6814b7dc37e95
Ju Liu <liuju86@gmail.com>
2016-09-19 23:53:45 +0000

    Add more sensors and use application

--- alarm/lib/alarm.ex
+++ alarm/lib/alarm.ex
@@ -1,38 +1,60 @@
 defmodule Alarm do
-  @timeout 200
+  use Application
+
+  @timeout 100
   @laser_pin 17
   @buzzer_pin 18
 
-  def start_link do
+  def start(_, _) do
     {:ok, laser} = Gpio.start_link(@laser_pin, :output)
     Gpio.write(laser, 0)
 
     {:ok, buzzer} = Gpio.start_link(@buzzer_pin, :output)
     Gpio.write(buzzer, 1)
 
-    {:ok, sensor} = I2c.start_link("i2c-1", 0x48)
+    {:ok, sensors} = I2c.start_link("i2c-1", 0x48)
 
-    loop(%{laser: laser, buzzer: buzzer, sensor: sensor})
+    loop(%{buzzer: buzzer, sensors: sensors})
   end
 
-  def loop(%{laser: laser, buzzer: buzzer, sensor: sensor} = state) do
+  def loop(%{buzzer: buzzer, sensors: sensors} = state) do
     :timer.sleep(@timeout)
 
-    value = read_sensor(sensor)
+    value = read_sensor(sensors, 0)
+
     if value > 100 do
-      IO.puts "intruder!"
+      IO.puts "light: intruder! (value #{value})"
       Gpio.write(buzzer, 0)
-    else
-      IO.puts "all good"
-      Gpio.write(buzzer, 1)
+      loop(state)
     end
+
+    value = read_sensor(sensors, 1)
+
+    if value < 110 do
+      IO.puts "temperature: intruder! (value #{value})"
+      Gpio.write(buzzer, 0)
+      loop(state)
+    end
+
+    value = read_sensor(sensors, 2)
+
+    if value < 110 do
+      IO.puts "sound: intruder! (value #{value})"
+      Gpio.write(buzzer, 0)
+      loop(state)
+    end
+
+    # no alarm failing
+    Gpio.write(buzzer, 1)
 
     loop(state)
   end
 
-  defp read_sensor(sensor) do
-    I2c.read(sensor, 1)
-    <<value>> = I2c.read(sensor, 1)
+  defp read_sensor(pid, channel) do
+    {channel_value, _} = Integer.parse("#{channel + 40}", 16)
+    I2c.write(pid, <<channel_value>>)
+    I2c.read(pid, 1)
+    <<value>> = I2c.read(pid, 1)
     value
   end
 end
